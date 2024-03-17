9e8f510c468259b03736c6afcda0948ef4d5bff5
Shahaf Lalo <shahaf.lalo@post.runi.ac.il>
2024-03-17 18:08:35 +0200

    my vers

--- SpellChecker.java
+++ SpellChecker.java
@@ -12,50 +12,52 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		String tail = "";
-		for (int i = 1; i < str.length(); i++) {
-		tail += str.charAt(i);
-		}
+		String tail = str.substring(1);
 		return tail;
+	}
 
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		String low1 = word1.toLowerCase();
-		String low2 = word2.toLowerCase();
+		String lWord1=word1.toLowerCase();
+		String lWord2=word2.toLowerCase();
 
-		if (low2.length() == 0)  
-			return low1.length();
+		if (lWord2.length()==0){
+			return lWord1.length();
+		}
 
-		if (low1.length() == 0)  
-			return low2.length();
-		
-		if (low1.charAt(0) == low2.charAt(o)) {
-			return (levenshtein(tail(low1),tail(low2)));
+		if (lWord1.length()==0){
+			return lWord2.length();
+		}
+
+		if (lWord1.charAt(0)==lWord2.charAt(0)){
+			return levenshtein(tail(lWord1),tail(lWord2));
 
 		} else {
-			return 1 + (math.min(levenshtein(tail(low1),low2)),math.min((levenshtein(tail(low2),low1)),levenshtein(tail(low1),tail(low2))))
+		return 1+(Math.min(Math.min(levenshtein(tail(lWord1), lWord2),levenshtein(lWord1,tail(lWord2))),levenshtein(tail(lWord1),tail(lWord2))));
 		}
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
-		In in = new In(fileName);
-
-		// Your code here
-
+		In in = new In("dictionary.txt");
+			for(int i=0;i<3000;i++){
+				dictionary[i]=in.readLine();
+			}
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		int minT = 100;
-		for (int i = 0; i < dictionary.length; i++)
-		if (levenshtein( dictionary[i], word )<= threshold && levenshtein( dictionary[i], word ) < minT ){
-			String checked = dictionary[i];
-			minT = (levenshtein( dictionary[i], word)
+		int minTh = 100;
+		word = word.toLowerCase();
+		String match = word;
+		for (int i=0;i<dictionary.length;i++){
+			if (levenshtein(dictionary[i], word)<minTh&&levenshtein(dictionary[i], word)<=threshold) {
+				match=dictionary[i];
+				minTh=levenshtein(dictionary[i], word);
+			}
 		}
-		return checked;
+	return match;
 	}
-
+	
 }
