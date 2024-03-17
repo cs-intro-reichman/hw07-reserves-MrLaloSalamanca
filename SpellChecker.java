2c3e3231547f91fd0e9e2703f56f8e3a7f2f0f9a
Shahaf Lalo <shahaf.lalo@post.runi.ac.il>
2024-03-17 18:12:27 +0200

    18:12

--- SpellChecker.java
+++ SpellChecker.java
@@ -3,12 +3,11 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
-		String word1 = args[0];
+		String word = args[0];
 		int threshold = Integer.parseInt(args[1]);
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
-		
 	}
 
 	public static String tail(String str) {
@@ -16,24 +15,19 @@ public class SpellChecker {
 		return tail;
 	}
 
-	}
-
 	public static int levenshtein(String word1, String word2) {
 		String lWord1=word1.toLowerCase();
 		String lWord2=word2.toLowerCase();
-
 		if (lWord2.length()==0)
 			return lWord1.length();
-
 		if (lWord1.length()==0)
 			return lWord2.length();
-
 		if (lWord1.charAt(0)==lWord2.charAt(0))
 			return levenshtein(tail(lWord1),tail(lWord2));
-			
 		else
 		return 1+(Math.min(Math.min(levenshtein(tail(lWord1), lWord2),levenshtein(lWord1,tail(lWord2))),levenshtein(tail(lWord1),tail(lWord2))));
 	}
+
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 		In in = new In("dictionary.txt");
