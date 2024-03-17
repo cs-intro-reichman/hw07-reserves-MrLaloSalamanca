9491620632ea3b989001c832d46caa1cec28c863
Shahaf Lalo <shahaf.lalo@post.runi.ac.il>
2024-03-17 18:04:16 +0200

    my vers

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -6,35 +6,48 @@ public class HashTagTokenizer {
 
 		String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
+		//System.out.println("the word is in the string? " + existInDictionary(hashTag, dictionary));
 		breakHashTag(hashTag, dictionary);
 	}
 
+	
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i = 0; i < dictionary.length; i++){
+			dictionary[i] = in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++){
+			if (dictionary[i].equals(word)){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
-		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
-            return;
-        }
- 
-        int N = hashtag.length();
+		// convert the entire hashtag to lowercase
+		String lowHash = hashtag.toLowerCase();
+		
+		int N = lowHash.length();
+	
+		// if the hastag is empty
+        if (lowHash.isEmpty()){
+			return;
+		}
 
         for (int i = 1; i <= N; i++) {
-		
-        }
-    }
+			String sub = lowHash.substring(0, i);
+			if (existInDictionary(sub, dictionary)) {
+				System.out.println(sub);
+				breakHashTag(lowHash.substring(i), dictionary);
+				break;
+			}
+		}
+	}
 
 }
--- SpellChecker.java
+++ SpellChecker.java
@@ -3,19 +3,39 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
-		String word = args[0];
+		String word1 = args[0];
 		int threshold = Integer.parseInt(args[1]);
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
+		
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		String tail = "";
+		for (int i = 1; i < str.length(); i++) {
+		tail += str.charAt(i);
+		}
+		return tail;
+
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		String low1 = word1.toLowerCase();
+		String low2 = word2.toLowerCase();
+
+		if (low2.length() == 0)  
+			return low1.length();
+
+		if (low1.length() == 0)  
+			return low2.length();
+		
+		if (low1.charAt(0) == low2.charAt(o)) {
+			return (levenshtein(tail(low1),tail(low2)));
+
+		} else {
+			return 1 + (math.min(levenshtein(tail(low1),low2)),math.min((levenshtein(tail(low2),low1)),levenshtein(tail(low1),tail(low2))))
+		}
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -29,7 +49,13 @@ public class SpellChecker {
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		int minT = 100;
+		for (int i = 0; i < dictionary.length; i++)
+		if (levenshtein( dictionary[i], word )<= threshold && levenshtein( dictionary[i], word ) < minT ){
+			String checked = dictionary[i];
+			minT = (levenshtein( dictionary[i], word)
+		}
+		return checked;
 	}
 
 }
